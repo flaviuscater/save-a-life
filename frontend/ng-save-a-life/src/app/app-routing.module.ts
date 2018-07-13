74a52b6f71e8b63d7c96f4fe13576ccd88c3148e
unknown <flavius.cater@gmail.com>
2018-07-13 12:53:33 +0300

    Trying to add background image

--- frontend/ng-save-a-life/src/app/app-routing.module.ts
+++ frontend/ng-save-a-life/src/app/app-routing.module.ts
@@ -8,7 +8,6 @@ import {HospitalInformationComponent} from "./components/hospital-information/ho
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'about', component: AboutComponent},
-  { path: 'info', component: HospitalInformationComponent}
 ];
 
 @NgModule({
